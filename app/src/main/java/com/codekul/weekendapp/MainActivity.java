a8d0897a3c46b79ee8f60726303a7e75a5cf4323
aniruddha <aniruddha.kudalkar@gmail.com>
2016-12-10 18:29:48 +0530

    here I have created simple android UI using java style.

--- app/src/main/java/com/codekul/weekendapp/MainActivity.java
+++ app/src/main/java/com/codekul/weekendapp/MainActivity.java
@@ -1,13 +1,30 @@
 package com.codekul.weekendapp;
 
+import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        LinearLayout.LayoutParams paramsRoot
+                = new LinearLayout.LayoutParams(300,300);
+        LinearLayout layoutRoot = new LinearLayout(this);
+        layoutRoot.setLayoutParams(paramsRoot);
+        layoutRoot.setOrientation(LinearLayout.VERTICAL);
+        setContentView(layoutRoot);
+
+        LinearLayout.LayoutParams paramsBtn
+                = new LinearLayout.LayoutParams(300,150);
+        Button btnOkay = new Button(this);
+        btnOkay.setText("Okay");
+        layoutRoot.addView(btnOkay);
     }
 }

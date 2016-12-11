8dcb567e93119d02bcb5e18740d3d988ac99904d
aniruddha <aniruddha.kudalkar@gmail.com>
2016-12-11 17:21:31 +0530

    creating ui through xml, and button click listeners, java8 integration, padding , marging, view measurement units

--- uixml/src/main/java/com/codekul/uixml/MainActivity.java
+++ uixml/src/main/java/com/codekul/uixml/MainActivity.java
@@ -2,13 +2,60 @@ package com.codekul.uixml;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        //Click cl = new Click();
+        final Button btnOkay = (Button) findViewById(R.id.btnOkay);
+        //btnOkay.setOnClickListener(cl);
+        //findViewById(R.id.btnReset).setOnClickListener(cl);
+
+        btnOkay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView textCodeKul = (TextView) findViewById(R.id.textCodeKul);
+                textCodeKul.setText("{code}kul;");
+            }
+        });
+
+        findViewById(R.id.btnReset).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textCodeKul = (TextView) findViewById(R.id.textCodeKul);
+                textCodeKul.setText("codekul");
+            }
+        });
+
+        findViewById(R.id.btnUpperCase).setOnClickListener( v -> ((TextView) findViewById(R.id.textCodeKul)).setText("codekul".toUpperCase()) );
+
+        findViewById(R.id.btnLowerCase).setOnClickListener(this::loweCase);
+    }
+
+    private void loweCase(View view) {
+        if(view.getId() == R.id.btnLowerCase){
+            ((TextView) findViewById(R.id.textCodeKul)).setText("codekul");
+        }
+    }
+
+    private class Click implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            if(v.getId() == R.id.btnOkay){
+
+            }
+            else {
+
+            }
+        }
     }
 }

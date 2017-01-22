b0949a2e0acdf597960b4676709f808950a61e04
aniruddha <aniruddha.kudalkar@gmail.com>
2017-01-22 17:42:20 +0530

    content provider - reading contacts

---
+++ contentproviders/src/main/java/com/melayer/contentproviders/MainActivity.java
@@ -0,0 +1,37 @@
+package com.melayer.contentproviders;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final List<String> contacts = new ArrayList<>();
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME};
+        String selection = null;
+        String[] selectionArgs = null;
+        String sortOrder = null;
+
+        Cursor contactCursor = getContentResolver().query(uri, projection, selection, selectionArgs, sortOrder);
+
+        while(contactCursor.moveToNext()) {
+            String num = contactCursor.getString(contactCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            String name = contactCursor.getString(contactCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            contacts.add(num + "\n" +name);
+        }
+        ((ListView)findViewById(R.id.listContacts)).setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, contacts));
+    }
+}
